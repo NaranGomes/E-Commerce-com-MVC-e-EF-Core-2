fc5e83d0b968e2c1c0395c1a197c20f7455fd474
Naran Gomes <gom3snaran@gmail.com>
2022-02-08 16:59:37 -0300

    Criada classe CarrinhoViewModel

---
+++ Aulas/Aula3/antes/CasaDoCodigo/Models/ViewModels/CarrinhoViewModel.cs
@@ -0,0 +1,18 @@
+﻿using CasaDoCodigo.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CasaDoCodigo.aula3.antes.Models.ViewModels
+{
+    public class CarrinhoViewModel
+    {
+        public IList<ItemPedido> Itens { get; }
+
+        public CarrinhoViewModel(IList<ItemPedido> itens)
+        {
+            Itens = itens;
+        }
+
+        public decimal Total => Itens.Sum(i => i.Quantidade * i.PrecoUnitario);
+    }
+}
