79533aaa42b5d0a8f65ea8c4f5ac4b9618a74f2c
Naran Gomes <gom3snaran@gmail.com>
2022-02-08 20:21:12 -0300

    Criada classe UpdateQuantidadeResponse que conterá o objeto de resposta para a chamada AJAX

---
+++ Aulas/Aula3/antes/CasaDoCodigo/Models/UpdateQuantidadeResponse.cs
@@ -0,0 +1,11 @@
+﻿using CasaDoCodigo.aula3.antes.Models.ViewModels;
+using CasaDoCodigo.Models;
+
+namespace CasaDoCodigo.aula3.antes.Models
+{
+    public class UpdateQuantidadeResponse
+    {
+        public ItemPedido ItemPedido { get; }
+        public CarrinhoViewModel CarrinhoViewModel { get; } 
+    }
+}
